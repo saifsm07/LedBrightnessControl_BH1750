f6cf9db888457a167f454a83d8d32bd99de0857e
Ahmad Saif <80459847+saifsm07@users.noreply.github.com>
2021-05-30 20:09:08 +0530

    Add files via upload

---
+++ LedBrightnessControl_BH1750.ino
@@ -0,0 +1,29 @@
+#include<Wire.h> 
+#include <BH1750.h>  
+
+int LED = 7;
+
+BH1750 GY30;
+
+void setup()
+{
+  Serial.begin(9600); 
+  Wire.begin(); 
+  GY30.begin();
+  Serial.println("SAIF LX WORKSHOP");
+  pinMode(LED,OUTPUT);
+}
+void loop()
+{
+  float lux = GY30.readLightLevel(); 
+  Serial.println((String)"Light: " + lux + " lx"); 
+  delay(1000); 
+  if(lux<50)
+  {
+    digitalWrite(LED,HIGH);
+  }
+  else
+  {
+    digitalWrite(LED,LOW);
+  }
+} 
